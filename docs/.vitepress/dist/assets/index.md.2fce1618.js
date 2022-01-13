449fb6ac399f0c7c32415e4023876d72d2fec296
Carrie <carriehan@vesync.com.cn>
2022-01-13 21:36:09 +0800

    build

---
+++ docs/.vitepress/dist/assets/index.md.2fce1618.js
@@ -0,0 +1,1 @@
+import{u as e,c as t,e as a,o as s,C as n}from"./app.1f6c56c2.js";const d='{"title":"Home","description":"","frontmatter":{"title":"xxx","editLink":true,"home":true},"relativePath":"index.md","lastUpdated":1642046017631}',o={},l=Object.assign(o,{setup(_){return e(),(r,c)=>(s(),t("div",null,[a(n)]))}});export{d as __pageData,l as default};
---
+++ docs/.vitepress/dist/assets/operations_index.md.6fb0e6ce.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.1f6c56c2.js";const f='{"title":"","description":"","frontmatter":{},"relativePath":"base/index.md","lastUpdated":1640947007042}',r={};function c(o,s,n,p,d,i){return a(),t("div")}var l=e(r,[["render",c]]);export{f as __pageData,l as default};
